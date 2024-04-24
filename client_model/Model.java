0ce864e2f063e0576b4f04e7ad1ef70c86555fe7
katiekasia <olejarczykkatarzyna2022@gmail.com>
2024-04-24 11:21:10 +0200

    Model interface on Client

--- client_model/Model.java
+++ client_model/Model.java
@@ -7,4 +7,13 @@ public interface Model extends UnnamedPropertyChangeSubject
   public void bookTicket(Ticket ticket, User user);
   public void logIn(User user);
   public void cancelTicket(Ticket ticket);
-}
+  public void connect();
+  public void disconnect();
+  public int getPort();
+  public void setPort(int port);
+  public String getHost();
+  public void reserveSeats(Seat[] seats,User customer,ScreeningDay day, Screening screening );
+
+  public void reserveSeat(Seat seat, User customer,Screening screening);
+
+  }
--- client_model/ModelManager.java
+++ client_model/ModelManager.java
@@ -36,4 +36,28 @@ public class ModelManager implements Model
   {
 
   }
+  @Override public void reserveSeat(Seat seat, User customer,Screening screening){
+    if (screening.getRoom().availableSeats() <= 0){
+      throw new RuntimeException("No available seats left for this screening");
+    }
+    Order temp = new Order(customer.getOrders().size() + 1);
+    Ticket tempT = new StandardTicket(seat.getID(), 13,seat,screening,customer);
+    seat.book(tempT);
+    temp.addTicket(tempT);
+    customer.addOrder(temp);
+    System.out.println("booking complete");
+    System.out.println(temp + "\n" + temp.getTickets().size());
+  }
+  @Override public void reserveSeats(Seat[] seats,User customer,ScreeningDay day, Screening screening ){
+    if (screening.getRoom().availableSeats() < seats.length){
+      throw new RuntimeException("Not enough available seats left for this screening");
+    }
+    Order temp = new Order(customer.getOrders().size() + 1);
+    for (Seat seat:seats){
+      Ticket tempT = new StandardTicket(seat.getID(), 13,seat,screening,customer);
+      seat.book(tempT);
+      temp.addTicket(tempT);
+    }
+    customer.addOrder(temp);
+  }
 }
