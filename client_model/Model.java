468a8e2d63f2980b07eadaf1d5b2cb6392d4bc15
katiekasia <olejarczykkatarzyna2022@gmail.com>
2024-04-24 12:49:41 +0200

    Model Manager

--- client_model/Model.java
+++ client_model/Model.java
@@ -4,17 +4,22 @@ import utility.observer.javaobserver.UnnamedPropertyChangeSubject;
 
 public interface Model extends UnnamedPropertyChangeSubject
 {
-  public void bookTicket(Ticket ticket, User user);
   public void logIn(User user);
-  public void cancelTicket(Ticket ticket);
   public void connect();
   public void disconnect();
   public int getPort();
   public void setPort(int port);
   public String getHost();
-  
   public void reserveSeats(Seat[] seats,User customer,ScreeningDay day, Screening screening );
 
   public void reserveSeat(Seat seat, User customer,Screening screening);
+  public boolean checkSeatAvailability(int index, Screening screening);
+  boolean checkSeatAvailability(int index, Screening screening);
+  public Seat[] getAvailableSeats(Screening screening);
+  public boolean holdSeat(Seat seat, User customer, Screening screening);
+  public void releaseHoldSeat(Seat seat, User customer, Screening screening);
+
+  public double calculateTotalPrice(Seat[] seats, Screening screening);
+  public void updateSeatStatus(Seat seat);
 
   }
--- client_model/ModelManager.java
+++ client_model/ModelManager.java
@@ -1,7 +1,5 @@
 package client_model;
 
-import client_model.Model;
-
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
@@ -9,6 +7,12 @@ import java.beans.PropertyChangeSupport;
 // then press Enter. You can now see whitespace characters in your code.
 public class ModelManager implements Model
 {
+  private String HOST;
+  private int PORT;
+  private boolean running;
+
+  //not sure if the user variable is the one connected here
+  private User user;
   private PropertyChangeSupport propertyChangeSupport;
 
   @Override public void addListener(PropertyChangeListener listener)
@@ -22,39 +26,98 @@ public class ModelManager implements Model
     propertyChangeSupport.removePropertyChangeListener(listener);
   }
 
-  @Override public void bookTicket(Ticket ticket, User customer)
-  {
-
-  }
-
   @Override public void logIn(User customer)
   {
-
+    //not for this sprint
   }
 
-  @Override public void cancelTicket(Ticket ticket)
+  @Override public void connect()
   {
-
+    //
   }
-  @Override public void reserveSeat(Seat seat, User customer,Screening screening){
-    if (screening.getRoom().availableSeats() <= 0){
+
+  @Override public void disconnect()
+  {
+    //
+  }
+
+  @Override public int getPort()
+  {
+    return PORT;
+  }
+
+  @Override public void setPort(int port)
+  {
+    this.PORT = port;
+  }
+
+  @Override public String getHost()
+  {
+    return HOST;
+  }
+
+  @Override public void reserveSeat(Seat seat, User customer,
+      Screening screening)
+  {
+    if (screening.getRoom().availableSeats() <= 0)
+    {
       throw new RuntimeException("No available seats left for this screening");
     }
     Order temp = new Order(customer.getOrders().size() + 1);
-    Ticket tempT = new StandardTicket(seat.getID(), 13,seat,screening,customer);
+    Ticket tempT = new StandardTicket(seat.getID(), 13, seat, screening,
+        customer);
     seat.book(tempT);
     temp.addTicket(tempT);
     customer.addOrder(temp);
     System.out.println("booking complete");
     System.out.println(temp + "\n" + temp.getTickets().size());
   }
-  @Override public void reserveSeats(Seat[] seats,User customer,ScreeningDay day, Screening screening ){
-    if (screening.getRoom().availableSeats() < seats.length){
-      throw new RuntimeException("Not enough available seats left for this screening");
+
+  @Override public boolean checkSeatAvailability(int index, Screening screening)
+  {
+    return (screening.getRoom().getSeat(index).isAvailable());
+  }
+
+  @Override public Seat[] getAvailableSeats(Screening screening)
+  {
+    return new Seat[0];
+  }
+
+  @Override public boolean holdSeat(Seat seat, User customer,
+      Screening screening)
+  {
+    return false;
+  }
+
+  @Override public void releaseHoldSeat(Seat seat, User customer,
+      Screening screening)
+  {
+
+  }
+
+  @Override public double calculateTotalPrice(Seat[] seats, Screening screening)
+  {
+    return 0;
+  }
+
+  @Override public void updateSeatStatus(Seat seat)
+  {
+
+  }
+
+  @Override public void reserveSeats(Seat[] seats, User customer,
+      ScreeningDay day, Screening screening)
+  {
+    if (screening.getRoom().availableSeats() < seats.length)
+    {
+      throw new RuntimeException(
+          "Not enough available seats left for this screening");
     }
     Order temp = new Order(customer.getOrders().size() + 1);
-    for (Seat seat:seats){
-      Ticket tempT = new StandardTicket(seat.getID(), 13,seat,screening,customer);
+    for (Seat seat : seats)
+    {
+      Ticket tempT = new StandardTicket(seat.getID(), 13, seat, screening,
+          customer);
       seat.book(tempT);
       temp.addTicket(tempT);
     }
