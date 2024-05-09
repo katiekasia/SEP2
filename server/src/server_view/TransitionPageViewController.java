16fb864ab9ceeb49b240b2b330a9d4d68bbca58f
CatalinaTonu <catalinatonu9@gmail.com>
2024-05-09 13:19:33 +0200

    some little changes in tgransition page

--- server/src/server_view/TransitionPageViewController.java
+++ server/src/server_view/TransitionPageViewController.java
@@ -23,6 +23,8 @@ public class TransitionPageViewController
   @FXML private Label movieDate;
   @FXML private Label movieTime;
   @FXML private Label roomID;
+  @FXML private ChoiceBox<Integer> numberOfStandartTickets;
+  @FXML private ChoiceBox<Integer> numberOfVIPTickets;
   @FXML private Button fidelityPoints;
   @FXML private Button manage;
   @FXML private Button signOut;
@@ -43,8 +45,24 @@ movieDate.setText(viewState.getSelectedScreening().dateProperty().get());
 movieTime.setText(viewState.getSelectedScreening().timeProperty().get());
 roomID.setText(String.valueOf(viewState.getSelectedScreening().roomProperty().get()));
 
+      initializeTicketChoiceBoxes();
     }
+  private void initializeTicketChoiceBoxes() {
+    numberOfStandartTickets.getItems().addAll(0, 1, 2, 3, 4, 5); // or any other logic
+    numberOfVIPTickets.getItems().addAll(0, 1, 2, 3, 4, 5); // or any other logic
 
+    // Add listeners to update the total price
+    numberOfStandartTickets.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> updateTotal());
+    numberOfVIPTickets.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> updateTotal());
+  }
+
+  private void updateTotal() {
+    int standardTickets = numberOfStandartTickets.getValue() != null ? numberOfStandartTickets.getValue() : 0;
+    int vipTickets = numberOfVIPTickets.getValue() != null ? numberOfVIPTickets.getValue() : 0;
+
+    int total = standardTickets * 120 + vipTickets * 170;
+    totalPrice.setText(String.valueOf(total) + " DKK");
+  }
   @FXML public void onManage()
   {
 
