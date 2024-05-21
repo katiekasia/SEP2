040939f56dd1b6b196fda079e0867da60690129c
SaibotGames <mkbarczuk@wp.pl>
2024-05-21 13:25:56 +0200

    changes

--- server/src/model/Model.java
+++ server/src/model/Model.java
@@ -33,10 +33,7 @@ public interface Model extends UnnamedPropertyChangeSubject
 
 User getUserByUsername(String username);
   public void updateSeatToBooked(Seat seat, Ticket ticket);
-
-<<<<<<< Updated upstream
   public void addOrder(Order order, User user);
-  void register(String username, String password, String email, String firstName, String lastName, String phone);
 void addScreening(Screening screening);
 void removeScreening(Screening screening);
 void removeByDate(SimpleDate date);
@@ -51,21 +48,6 @@ Screening getScreening(Screening screening);
   ArrayList<Screening> getScreaningsByMovieTitle(String title);
   ArrayList<Screening> getScreeningsByDate(LocalDate date);
   ArrayList<Screening> getScreeningsByDateAndTitle(String title, LocalDate date);
-
-=======
-  public void addOrder(Order order) throws RemoteException;
-void addScreening(Screening screening) throws RemoteException;
-void removeScreening(Screening screening) throws RemoteException;
-void removeByDate(SimpleDate date) throws RemoteException;
-ArrayList<Screening> getAllScreenings() throws RemoteException;
-int getNbOfScreenings() throws RemoteException;
-Screening getScreening(Screening screening) throws RemoteException;
-  public ArrayList<Ticket> getAllTickets() throws RemoteException;
-  public User getUser() throws RemoteException;
-  Screening findScreeningBySeatId(String seatId) throws RemoteException;
-  ArrayList<Screening> getScreaningsByMovieTitle(String title) throws RemoteException;
-  ArrayList<Screening> getScreeningsByDate(LocalDate date) throws RemoteException;
->>>>>>> Stashed changes
   public ArrayList<Order> getOrdersForUser(String username);
   void register(String username, String password, String email, String firstName, String lastName, String phone) throws RemoteException;
 
