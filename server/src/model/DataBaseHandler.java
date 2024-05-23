600abd7845332b913573708b23adcbb77e06895c
Saskosaneas <chilatsandu@gmail.com>
2024-05-23 14:09:57 +0200

    backup

--- server/src/model/DataBaseHandler.java
+++ server/src/model/DataBaseHandler.java
@@ -13,7 +13,7 @@ public class DataBaseHandler
 {
   private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
   private static final String USERNAME = "postgres";
-  private static final String PASSWORD = "papiezpolak";
+  private static final String PASSWORD = "7890";
 
   private static Connection connection;
 
