04ec5f56591468f7304c5b5489df880cf0fb88a0
SaibotGames <mkbarczuk@wp.pl>
2024-05-27 16:38:42 +0200

    IDK ANYMORE

---
+++ server/src/viewmodel/AddMovieViewModel.java
@@ -0,0 +1,23 @@
+package viewmodel;
+
+import model.Model;
+
+import java.time.LocalDate;
+
+public class AddMovieViewModel {
+  private Model model;
+  private ViewState viewState;
+
+  public AddMovieViewModel(Model model, ViewState viewState) {
+    this.model = model;
+    this.viewState = viewState;
+  }
+
+  public ViewState getViewState() {
+    return viewState;
+  }
+
+  public void addMovie(String name, String length, String description, String genre, LocalDate releaseDate) {
+    model.addMovie(name, length, description, genre, releaseDate);
+  }
+}
---
+++ server/src/viewmodel/AddScreeningViewModel.java
@@ -0,0 +1,33 @@
+package viewmodel;
+
+import model.Model;
+import model.Movie;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class AddScreeningViewModel
+{
+  private Model model;
+  private ViewState viewState;
+
+  public AddScreeningViewModel(Model model, ViewState viewState)
+  {
+    this.model = model;
+    this.viewState = viewState;
+  }
+  public ViewState getViewState()
+  {
+    return viewState;
+  }
+
+
+  public ArrayList<Movie> getAllMovies() {
+      return model.getAllMovies();
+}
+
+public void addScreening(int hour, int minute, LocalDate date, String movieTitle, int roomID)
+{
+model.newScreening(hour,minute,date,movieTitle,roomID);
+}
+  }
