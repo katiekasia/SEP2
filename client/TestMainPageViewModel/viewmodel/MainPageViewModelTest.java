2fab06e4de00f4fdaa40238088e8ddcc0b253798
katiekasia <olejarczykkatarzyna2022@gmail.com>
2024-05-29 13:43:53 +0200

    test cases

---
+++ client/TestMainPageViewModel/viewmodel/MainPageViewModelTest.java
@@ -0,0 +1,65 @@
+package viewmodel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainPageViewModelTest
+{
+
+  @Test void inputProperty()
+  {
+  }
+
+  @Test void setScreenings()
+  {
+  }
+
+  @Test void setCurrent()
+  {
+  }
+
+  @Test void bindScreenings()
+  {
+  }
+
+  @Test void setSelected()
+  {
+  }
+
+  @Test void getViewState()
+  {
+  }
+
+  @Test void loadScreenings()
+  {
+  }
+
+  @Test void clearFilters()
+  {
+  }
+
+  @Test void onSearch()
+  {
+  }
+
+  @Test void filterByTitle()
+  {
+  }
+
+  @Test void filterByDate()
+  {
+  }
+
+  @Test void propertyChange()
+  {
+  }
+
+  @Test void addListener()
+  {
+  }
+
+  @Test void removeListener()
+  {
+  }
+}
